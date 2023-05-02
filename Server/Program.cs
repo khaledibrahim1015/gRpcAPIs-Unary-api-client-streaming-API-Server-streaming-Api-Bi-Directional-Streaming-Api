39f64334aee15ccfdb61f7e8ffb48598989a42a3
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2023-05-03 01:15:26 +0300

    create client and server projects and Grpc Configuration and test protocolBuffer compiled success

---
+++ Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using System.IO;
+namespace server
+{
+    internal class Program
+    {
+        const int port = 50051;
+        static void Main(string[] args)
+        {
+            Server server = null;
+            try
+            {
+
+                server = new Server()
+                {
+                    Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+                };
+                server.Start();
+                Console.WriteLine("The Server is listening on the port "+port);
+
+
+
+
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The Server is Failed to start : "+e.Message);
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                    server.ShutdownAsync().Wait();
+            }
+        }
+    }
+}
