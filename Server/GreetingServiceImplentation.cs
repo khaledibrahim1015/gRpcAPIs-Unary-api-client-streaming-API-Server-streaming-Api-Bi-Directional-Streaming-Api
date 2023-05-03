3ba2adcad399bbc925f43545f5b9c3a3add00c74
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2023-05-03 18:10:00 +0300

    Impelemet server streaming => Server side

--- Server/GreetingServiceImplentation.cs
+++ Server/GreetingServiceImplentation.cs
@@ -1,6 +1,7 @@
 ﻿using Greet;
 using Grpc.Core;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,52 @@ namespace server
             {
                 Result = string.Format($"Hello {request.Greeting.FirstName} {request.Greeting.LastName} ")
             });
+
+        }
+
+        // Implement server streaming 
+
+        /// <param name="request">The request received from the client.</param>
+        /// <param name="responseStream">Used for sending responses back to the client.</param>
+        /// responses act as list<GreetingManyTimesResponse> 
+        /// response one GreetingManyTimesResponse One by one 
+        public override async Task GreetManyTimes(GreetingManytimesRequest request,
+            IServerStreamWriter<GreetingManyTimesResponse> responseStream,
+            ServerCallContext context)
+        {
+            // 1 -way 
+
+            // List<GreetingManyTimesResponse> greetingManyTimesResponses = new List<GreetingManyTimesResponse>()
+            //{
+            //    new GreetingManyTimesResponse()
+            //    {
+            //        Result=string.Format($"{request.Greeting.FirstName} {request.Greeting.LastName}")
+            //    },
+            //     new GreetingManyTimesResponse()
+            //    {
+            //        Result=string.Format($"{request.Greeting.FirstName} {request.Greeting.LastName}")
+            //    }
+
+            //};
+
+            // // Sending responses but one by one 
+
+            // foreach (var response in greetingManyTimesResponses)
+            // {
+            //   await  responseStream.WriteAsync(response);
+            // }
+
+            // another way 
+            Console.WriteLine($"The server recived one Request {request.Greeting.FirstName} , {request.Greeting.LastName}");
+
+
+            foreach (var i in Enumerable.Range(0,10))
+            {
+                await responseStream.WriteAsync(new GreetingManyTimesResponse() { Result = string.Format($"hello  {request.Greeting.FirstName} {request.Greeting.LastName}") });
+            }
+
+
+
 
         }
 
